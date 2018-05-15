0d88afe4c0b168946a6b35cc6858811c148356fe
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2018-05-15 13:36:01 +0300

    End of day 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
