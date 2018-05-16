9fe69555aeedc6a29def3379e495c7528f8dc0f2
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2018-05-16 13:10:32 +0300

    Changes to listsearch, added HttpService

--- src/app/contact/services/contact.service.ts
+++ src/app/contact/services/contact.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import {Contact} from '../contact';
+import {ContactHttpService} from './contact-http.service';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  contacts: Contact[];
-  constructor() {
-    this.contacts = [];
+  //contacts: Contact[];
+  constructor(private contactHttpService: ContactHttpService) {
+    /*this.contacts = [];
     this.contacts.push(new Contact(1, 'Timo', 'Makkonen'));
     this.contacts.push(new Contact(2, 'Pekka', 'Leppänen'));
-    this.contacts.push(new Contact(3, 'Mikko', 'Leminmies'));
+    this.contacts.push(new Contact(3, 'Mikko', 'Leminmies'));*/
   }
-
+/*
   getContacts() {
     return this.contacts;
-  }
+  }*/
+getContacts(): Observable<Contact[]> {
+  return this.contactHttpService.get();
+}
+
 }
