46d1fa599389b3ab8b81b3db8fcd85e35ae2646f
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2018-05-16 13:55:23 +0300

    Made use of environments and deleted obsolete information

--- src/app/contact/services/contact.service.ts
+++ src/app/contact/services/contact.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Contact} from '../contact';
 import {ContactHttpService} from './contact-http.service';
 import {Observable} from 'rxjs';
@@ -8,19 +8,12 @@ import {Observable} from 'rxjs';
 })
 export class ContactService {
 
-  //contacts: Contact[];
   constructor(private contactHttpService: ContactHttpService) {
-    /*this.contacts = [];
-    this.contacts.push(new Contact(1, 'Timo', 'Makkonen'));
-    this.contacts.push(new Contact(2, 'Pekka', 'Leppänen'));
-    this.contacts.push(new Contact(3, 'Mikko', 'Leminmies'));*/
+
   }
-/*
-  getContacts() {
-    return this.contacts;
-  }*/
-getContacts(): Observable<Contact[]> {
-  return this.contactHttpService.get();
-}
+
+  getContacts(): Observable<Contact[]> {
+    return this.contactHttpService.get();
+  }
 
 }
