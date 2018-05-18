1904ccc1378a3445faac0edce1ea60c83f89a811
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2018-05-18 10:39:45 +0300

    CSS ADDITIONS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {ContactHttpService} from './contact/services/contact-http.service';
 import {HttpClientModule} from '@angular/common/http';
 import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
 import {RouterModule, Routes} from '@angular/router';
+import {MaterialComponentsModule} from './ui/material-components/material-components.module';
 
 const appRoutes: Routes = [
   { path: 'contacts', component: ContactListComponent},
@@ -21,12 +22,14 @@ const appRoutes: Routes = [
     AppComponent,
     ContactListComponent,
     ContactDetailComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MaterialComponentsModule
   ],
   providers: [
     ContactService,
